ca4b9c9ccf9f22a6930f392fe88ba6add2c0a261
Dila Öykü Eyüboğlu <eyubogluoyku44@gmail.com>
2026-07-14 09:43:57 +0300

    feat: add reservation retrieval by ID and implement reservation cancellation by releasing the reserved seat and updating reservation status

--- src/main/java/com/oyku/event_reservation_api/controller/ReservationController.java
+++ src/main/java/com/oyku/event_reservation_api/controller/ReservationController.java
@@ -7,12 +7,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oyku.event_reservation_api.dto.event.EventResponse;
 import com.oyku.event_reservation_api.dto.reservation.ReservationCreateRequest;
 import com.oyku.event_reservation_api.dto.reservation.ReservationResponse;
 import com.oyku.event_reservation_api.service.AuthService;
@@ -50,4 +53,20 @@ public class ReservationController {
 		return ResponseEntity.ok(reservations);
 	}
 
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/{id}")
+	public ResponseEntity<ReservationResponse> getReservationById(@PathVariable Long id) {
+
+		ReservationResponse response = reservationService.getReservationById(id);
+
+		return ResponseEntity.ok(response);
+	}
+	
+
+	@PreAuthorize("hasRole('USER')")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteReservation(@PathVariable Long id){
+		reservationService.cancelReservation(id);
+		return ResponseEntity.noContent().build();
+	}
 }
